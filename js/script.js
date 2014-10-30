6f7410a155ce64979a04d6852954288210a889b3
acervantesc <acervantesc@gmail.com>
2014-10-30 17:53:33 -0400

    Working version

--- js/script.js
+++ js/script.js
@@ -1,7 +1,26 @@
 $("#sneakers").click(function() {
 
-    $("#question").text("What color are my socks?");
+    $("#question").text("Try this!");
 
     $("#red").css("visibility","visible");
+
+
+});
+
+$("#dress").click(function() {
+
+    $("#question").text("Try this!");
+
+    $("#blue").css("visibility","visible");
+    
+
+});
+
+$("#boots").click(function() {
+
+    $("#question").text("Try this!");
+
+    $("#brown").css("visibility","visible");
+    
 
 });
